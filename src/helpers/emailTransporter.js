0714a2bbaf9a1d3a7c215cf166f6b57c333b44c2
Sanduni Lakshika <kumarimasl.21@uom.lk>
2026-02-04 15:22:07 +0530

    remove console logs

--- src/helpers/emailTransporter.js
+++ src/helpers/emailTransporter.js
@@ -3,11 +3,6 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const createTransporter = () => {
-  console.log("DEBUG: Email User is:", process.env.EMAIL_USER);
-  console.log(
-    "DEBUG: Password Length is:",
-    process.env.EMAIL_PASS ? process.env.EMAIL_PASS.length : "undefined",
-  );
   return nodemailer.createTransport({
     service: "gmail",
     auth: {
