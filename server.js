02381fc78c81458c41413027936c4cab50a0933c
Sanduni Lakshika <kumarimasl.21@uom.lk>
2026-05-30 16:28:12 +0530

    added more cors options origins

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ app.use(express.json());
 // CORS Configuration
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-portfolio-domain.com"], // Update this when you deploy
+    origin: [
+      "http://localhost:5173",
+      "https://sanduni-lakshika-portfolio.vercel.app",
+      "https://sanduni-lakshika.site",
+      "https://www.sanduni-lakshika.site",
+    ], // Update this when you deploy
     methods: ["POST", "GET"],
   }),
 );
